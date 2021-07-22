1e2919d1738049eaa41ec2df1164aec5608fc061
MadhuSudhan <msbn5991.mb@gmail.com>
2021-07-22 19:05:48 +0530

    new otp

---
+++ OTPApp/OTPPage.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OTPApp
+{
+    public partial class OTPPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            try
+            {
+                Panel1.Visible = false;
+                Panel2.Visible = true;
+                Random rndm = new Random();
+                int value = rndm.Next(11111, 99999);
+                string dstnaddrs = "91" + TextBox1.Text;
+                string msg = " Your OTP is : " + value +
+                    "- Naan kalsiddu kanro #E Sala Cup Namde,(sent by MADDY Application)";
+                string msg1 = HttpUtility.UrlEncode(msg);
+
+                using (var wc = new WebClient())
+                {
+                    byte[] response = wc.UploadValues("https://api.textlocal.in/send/", new NameValueCollection()
+                {
+                        //2bgAg5Ag9HY-83XFdBqAz09X9i3rOUryA12Vo2eIEl
+                    {"apikey" , "NjE3MTU2NTM1MjQ5NjgzNzQyMzc1NDczNGIzOTMxNTE=" },
+                    {"numbers" , dstnaddrs },
+                    {"message" , msg1 },
+                    {"Sender" , "MADHUSUDHAN" }
+                });
+
+                    string res = System.Text.Encoding.UTF8.GetString(response);
+                    Session["OTP"] = value;
+                }
+
+
+
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TextBox2.Text == Session["OTP"].ToString())
+                {
+                    Label3.Visible = false;
+                    Panel2.Visible = false;
+                    Label2.Text = "Your mobile number has been verified successfully";
+
+                    string dstnaddrs = "91" + TextBox1.Text;
+                    string msg = "Your mobile number has been verified successfully.";
+                    string msg1 = HttpUtility.UrlEncode(msg);
+
+                    using (var wc = new WebClient())
+                    {
+                        byte[] response = wc.UploadValues("https://api.textlocal.in/send/", new NameValueCollection()
+                {
+                    {"apikey" , "g1nql5klz8k-lSYnGp0Kr5b4QS9XDdmYvWPyXHROZ4	" },
+                    {"numbers" , dstnaddrs },
+                    {"message" , msg1 },
+                    {"Sender" , "MADHUSUDHAN" }
+                });
+                    }
+                }
+                else
+                {
+                    Panel2.Visible = true;
+                    Label3.Text = "OTP is incorrect";
+                }
+            }
+
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
